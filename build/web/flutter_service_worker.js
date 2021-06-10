945dac336cf787f5891820963331ee30cd318f3f
gsio <gsio@protonmail.com>
2021-06-10 15:21:37 +0700

    Update version to 2.0.4

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "dfab097dc407fe1add59f2c929696208",
-"index.html": "dd9c6c5d4c5d3c938e99e2ffaa0635d3",
-"/": "dd9c6c5d4c5d3c938e99e2ffaa0635d3",
-"main.dart.js": "62dc132850e67b24b60ab936677f9417",
+"index.html": "c21d8b457d21e44b96a2a18d4395925a",
+"/": "c21d8b457d21e44b96a2a18d4395925a",
+"main.dart.js": "46617a5f131b6daa022b9240c0e63dc9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
